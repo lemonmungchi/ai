f2f3f5a8de35846deddc96217a692234ee31d834
Hyeon Chan <116485176+lemonmungchi@users.noreply.github.com>
2024-03-27 17:39:50 +0900

    1
    1

--- depth-estimation/app/src/main/java/com/samsung/depthestimation/data/DataType.kt
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2023 Samsung Electronics Co. LTD. Released under the MIT License.
-
-package com.samsung.depthestimation.data
-
-enum class DataType {
-    FLOAT32,
-    UINT8,
-    INT32,
-}
--- image-classification/app/src/main/java/com/samsung/imageclassification/data/DataType.kt
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2023 Samsung Electronics Co. LTD. Released under the MIT License.
-
-package com.samsung.imageclassification.data
-
-enum class DataType {
-    FLOAT32,
-    UINT8,
-    INT32,
-}
